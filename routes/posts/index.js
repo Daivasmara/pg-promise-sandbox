c41c899422b60d1b7a5ab303ebcdf4edb6eca6e5
Muhammad Daivasmara Denaw <Daivasmara@gmail.com>
2020-01-18 23:19:05 +0700

    changed single quotes to template literals

--- routes/posts/index.js
+++ routes/posts/index.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const db = require('../../db');
 
 router.get('/', async (_req, res) => {
-  const posts = await db.query('select p.id, email poster_email, first_name poster_first_name, title, body from posts p inner join users u on p.poster_id = u.id order by p.id desc');
+  const posts = await db.query(`
+    select p.id, email poster_email, first_name poster_first_name, title, body from posts p 
+    inner join users u on p.poster_id = u.id order by p.id desc
+  `);
   res.json(posts);
 });
 
--- routes/users/index.js
+++ routes/users/index.js
@@ -9,7 +9,12 @@ router.get('/', async (_req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  const user = await db.query('select u.*, coalesce(json_agg(json_build_object(\'title\', p.title, \'body\', p.body) order by p.id desc) filter (where p.title is not null), \'[]\') posts from users u left join posts p on u.id = p.poster_id where u.id = $1 group by u.id', [req.params.id]);
+  const user = await db.query(`
+      select u.*, coalesce(json_agg(json_build_object('title', p.title, 'body', p.body) order by p.id desc) 
+      filter (where p.title is not null), '[]') posts 
+      from users u left join posts p on u.id = p.poster_id 
+      where u.id = $1 group by u.id
+    `, [req.params.id]);
   res.json(user);
 });
 
